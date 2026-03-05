4bb399b524726b9b97c768dbcc774490df458445
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-03-05 22:16:31 +0300

    Clean repo

--- src/__test__/healthSort.test.js
+++ src/__test__/healthSort.test.js
@@ -16,21 +16,3 @@ test("healthSort simple", () => {
     ]        
     );
 });
-
-/*
-const dataList = [
-    [{name: "Max", health:100}, 'healthy'],
-    [{name: "Max", health:51}, 'healthy'],
-    [{name: "Max", health:50}, 'wounded'],
-    [{name: "Max", health:32}, 'wounded'],
-    [{name: "Max", health:15}, 'wounded'],
-    [{name: "Max", health:14}, 'critical'],
-    [{name: "Max", health:8}, 'critical']
-];
-
-const prefix = 'testing healthStatus for %s';
-test.each(dataList)(prefix, (myCharacter, status) => {
-    const result = getHealthStatus(myCharacter);
-    expect(result).toBe(status);
-});
-*/
