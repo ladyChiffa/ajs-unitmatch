70642e6f9ee8748ef6456951507c5efebd4c2426
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-03-10 11:18:48 +0300

    copy check

--- src/__test__/healthSort.test.js
+++ src/__test__/healthSort.test.js
@@ -16,3 +16,14 @@ test('healthSort simple', () => {
         ]        
     );
 });
+
+test('healthSort simple', () => {
+    const source = [
+        {name: 'Max', health:10}, 
+        {name: 'Leo', health:51},
+        {name: 'Sprite', health:15},
+        {name: 'Tom', health:32}
+    ];
+    const sorted = getHealthSorted(source);
+    expect(Object.is(sorted, source)).toBe(false);
+});
